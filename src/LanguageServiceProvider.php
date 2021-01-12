0600b847b908eb545701a9f5fa95ea8e568bcf00
Giorgi Gegetchkori <giorgi.gegetchkori.1@btu.edu.ge>
2021-01-13 00:58:51 +0400

    Update LanguageServiceProvider.php

--- src/LanguageServiceProvider.php
+++ src/LanguageServiceProvider.php
@@ -6,25 +6,10 @@ use Illuminate\Support\ServiceProvider;
 
 class LanguageServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../resources/lang/ka' => resource_path('lang/ka')
-        ]);
-    }
-
-    /**
-     * Register the application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
+            __DIR__.'/../resources/lang/ka' => resource_path('lang/ka'),
+        ],'lang');
     }
 }
