d237c202774966ae186d744776774946f8cfed04
gege17 <giorgi.gegetchkori.1@btu.edu.ge>
2021-01-12 21:58:29 +0400

    v1.0.0

---
+++ src/ka/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'თქვენი პაროლი განახლებულია!',
+    'sent' => 'ჩვენ ელ.ფოსტით გამოგიგზავნეთ თქვენი პაროლის განახლების ბმული!',
+    'throttled' => 'გთხოვთ, მოიცადოთ, სანამ ხელახლა ცდით.',
+    'token' => 'პაროლის განახლების კოდი არასწორია.',
+    'user' => "მომხმარებელი ამ ელ.ფოსტით ვერ იქნა ნაპოვნი.",
+
+];
